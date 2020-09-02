2afa2cbb865d71f7484dfb93d4440f37e13e4ce1
takyuchan <frankychantakyu2@gmail.com>
2020-09-02 19:08:07 +1200

    Learn Git please

---
+++ ourdailyapp/src/Pages/SignUpPage/SignUpPage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import S from "./SignUpPage.style";
+
+import SignUpForm from "../../Components/Forms/SignUpForm/SignUpForm.component";
+
+const SignUpPage = () => {
+  return (
+    <React.Fragment>
+      <SignUpForm />
+    </React.Fragment>
+  );
+};
+
+export default SignUpPage;
---
+++ ourdailyapp/src/Routers/AuthRouter/AuthRouter.component.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import S from "./AuthRouter.style";
+
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { Route, Switch, useRouteMatch, Redirect } from "react-router-dom";
+import { selectAuthPage } from "../../redux/AuthRouter/AuthRouter.selectors";
+import { changePage } from "../../redux/AuthRouter/AuthRouter.actions";
+
+import LogInPage from "../../Pages/LogInPage/LogInPage.component";
+import SignUpPage from "../../Pages/SignUpPage/SignUpPage.component";
+import SocialContactPair from "../../Components/SocialContact/SocialContactPair.component";
+
+import logo from "../../assets/logo_new.png";
+import { ReactComponent as LinkedInSvg } from "../../assets/svg/LinkedIn2.svg";
+import { ReactComponent as GithubSvg } from "../../assets/svg/GitHub2.svg";
+
+const AuthRouter = ({ authPage, changeAuthPage }) => {
+  function useRouter() {
+    const match = useRouteMatch();
+
+    return React.useMemo(() => {
+      return {
+        matchPath: match.path,
+      };
+    }, [match]);
+  }
+
+  const router = useRouter();
+
+  return (
+    <React.Fragment>
+      <S.LogInPageHazyBg></S.LogInPageHazyBg>
+      <S.LogInPageContent>
+        <S.LogoWrapper
+          className={authPage === "signup" && "styled_smallerLogo"}
+        >
+          <img className="logo" src={logo} alt="" role="presentation" />
+        </S.LogoWrapper>
+
+        {authPage === "login" && <LogInPage />}
+        {authPage === "signup" && <SignUpPage />}
+        {/* // ============== Create Account Btn ==============  */}
+        {authPage === "login" && (
+          <S.ToSignUpPage onClick={() => changeAuthPage("signup")}>
+            Create Account
+          </S.ToSignUpPage>
+        )}
+        {authPage === "signup" && (
+          <S.ToLogInPage onClick={() => changeAuthPage("login")}>
+            {`Log In Now ${router.matchPath}`}
+          </S.ToLogInPage>
+        )}
+
+        <S.SocialContactAndCopyRightWrapper>
+          {/* // ============== My Social Media Contact ==============  */}
+          <S.SocialContactWrapper>
+            <SocialContactPair SvgComponent={LinkedInSvg}>
+              LinkedIn
+            </SocialContactPair>
+            <SocialContactPair SvgComponent={GithubSvg}>
+              Github
+            </SocialContactPair>
+          </S.SocialContactWrapper>
+          {/* // ============== Copy Right Text ==============  */}
+          <S.CopyRightText>© 2020 by Franky Chan</S.CopyRightText>
+        </S.SocialContactAndCopyRightWrapper>
+      </S.LogInPageContent>
+    </React.Fragment>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  authPage: selectAuthPage,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeAuthPage: (pageName) => dispatch(changePage(pageName)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthRouter);
